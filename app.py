3cb9199f6662f98973c06f4ab71f538663eba263
k1ra057 <grdevg2.0@gmail.com>
2025-03-13 13:06:39 +0200

    adklskjldsaklasdklsadklлоьоло

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    print('Hello, World!')
+    return 'Hello, World!'  # Повертаємо відповідь клієнту
 
 if __name__ == "__main__":
     app.run(debug=True)
